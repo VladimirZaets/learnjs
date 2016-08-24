6af1044b4722ce6c74f9af111d73c2e63745f99c
Volodymyr Zaets <vzaets@magento.com>
2016-08-24 20:56:51 +0300

    LJS-14: Create NodeJS WebServer
    - Add main.html

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var express = require('express'),
     app = express();
 
+app.use('/', function (req, res) {
+        res.sendFile(__dirname + '/public/index.html');
+    }
+);
+
 module.exports = app;
