b6a8daf7358658ca406fbafaf6afabd6f244fe1f
Olha Nakonechna <onakonechna@magento.com>
2016-08-25 10:49:40 +0300

    LJS-12: Backend: Create application uses Express

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     app = express();
 
+app.use(express.static('node_modules'));
+app.use(express.static('public'));
 app.use('/', function (req, res) {
         res.sendFile(__dirname + '/public/index.html');
     }
